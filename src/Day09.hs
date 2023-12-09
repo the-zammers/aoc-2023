b213a7565656937e309c5b59fbe4bfa3731cfd45
Zameen Cater <zcater40@stuy.edu>
2023-12-09 22:03:57 +0000

    Day 9 done!

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,8 +1,29 @@
 module Day09 where
 
 import Paths_aoc2023 (getDataFileName)
+import Control.Arrow ((&&&))
+
+type Point = (Integer, Integer)
 
 day09 :: IO ()
 day09 = do
   contents <- init . lines <$> (getDataFileName "day09.txt" >>= readFile)
-  putStrLn $ unlines contents
+  print $ solve partA $ format contents -- 2098530125
+  print $ solve partB $ format contents -- 1016
+
+format :: [String] -> [[Point]]
+format = map $ zip [0..] . map read . words
+
+solve :: ([Point] -> Integer) -> [[Point]] -> Integer
+solve part = sum . map (\ls -> interpolate ls (part ls)) -- lambda left for clarity
+
+partA, partB :: [Point] -> Integer
+partA = length'
+partB = const (-1)
+
+interpolate :: [Point] -> Integer -> Integer
+interpolate ps x = sum $ map (uncurry ((*) . basis)) ps
+  where basis j = uncurry div $ (product . map (x-)) &&& (product . map (j-)) $ filter (/=j) $ [0 .. length' ps - 1]
+
+length' :: Foldable t => t a -> Integer
+length' = toInteger . length
