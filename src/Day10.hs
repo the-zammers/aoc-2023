ac6fd0f8f9e84449ba460e668c696b0b62445417
Zameen Cater <zcater40@stuy.edu>
2023-12-12 03:28:04 +0000

    Day 11 done! and day 10 cleaned

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,8 +1,11 @@
 module Day10 where
 
+-- todo: create a new sketch highlighting only the points that are in the loop instead of a list
+-- (elem is slow, ! is fast)
+
 import Paths_aoc2023 (getDataFileName)
-import ZArray -- would be WAY BETTER if I could get Data.Array working
 import Data.Maybe (fromJust)
+import Data.Array
 
 data Tile = NS | WE | NE | NW | SW | SE | X deriving (Eq)
 
@@ -15,37 +18,46 @@ goesE = flip elem [WE, NE, SE]
 tileList :: [Tile]
 tileList = [NS, WE, NE, NW, SW, SE, X]
 
-type Sketch = Array2 Tile
+type Coord2 = (Int, Int)
+type Sketch = Array Coord2 Tile
 
 day10 :: IO ()
 day10 = do
-  contents <- init . lines <$> (getDataFileName "day10b.txt" >>= readFile)
+  contents <- init . lines <$> (getDataFileName "day10.txt" >>= readFile)
   let formatted = format contents
   let origin = getOrigin formatted
   let cleaned = clean formatted origin
   print $ solveA cleaned origin
   print $ solveB cleaned origin
 
-format :: [[Char]] -> Array2 (Maybe Tile)
-format = fmap charToTile . convert2
+format :: [[Char]] -> Array Coord2 (Maybe Tile)
+format xs = fmap charToTile $ array ((0,0), (maxr-1, maxc-1)) [((r,c), xs!!r!!c) | r <- [0..maxr-1], c <- [0..maxc-1]]
   where charToTile = flip lookup $ zip "|-LJ7F." tileList
+        (maxr, maxc) = (length xs, length $ head xs)
 
-getOrigin :: Array2 (Maybe Tile) -> Coord2
-getOrigin = find Nothing
+getOrigin :: Array Coord2 (Maybe Tile) -> Coord2
+getOrigin xs = fromJust $ lookup Nothing $ map (\(i,e) -> (e,i)) $ assocs xs
 
-clean :: Array2 (Maybe Tile) -> Coord2 -> Sketch
-clean xs o = fmap fromJust $ replaceIndex xs o $ guessTile xs o
+clean :: Array Coord2 (Maybe Tile) -> Coord2 -> Sketch
+clean xs o = fmap fromJust $ xs // [(o, guessTile xs o)]
 
-guessTile :: Array2 (Maybe Tile) -> Coord2 -> Maybe Tile
+guessTile :: Array Coord2 (Maybe Tile) -> Coord2 -> Maybe Tile
 guessTile s pos = [toN, toS, toW, toE] `lookup` mapping
-  where toN = (==Just True) $ fmap (goesS . fromJust) $ safeIndex s $ stepN pos 
-        toS = (==Just True) $ fmap (goesN . fromJust) $ safeIndex s $ stepS pos 
-        toW = (==Just True) $ fmap (goesE . fromJust) $ safeIndex s $ stepW pos 
-        toE = (==Just True) $ fmap (goesW . fromJust) $ safeIndex s $ stepE pos 
+  where toN = (==Just True) $ goesS <$> s ! stepN pos 
+        toS = (==Just True) $ goesN <$> s ! stepS pos 
+        toW = (==Just True) $ goesE <$> s ! stepW pos 
+        toE = (==Just True) $ goesW <$> s ! stepE pos 
         mapping = zip (map (([goesN, goesS, goesW, goesE] <*>) . pure) tileList) tileList
 
+safeIndex :: Array (Int, Int) a -> Coord2 -> Maybe a
+safeIndex xs (r,c)
+  | r>=maxr || r<0 || c>=maxc || c<0 = Nothing
+  | otherwise = Just $ xs!(r,c)
+  where (maxr, maxc) = snd $ bounds xs
+
 solveA :: Sketch -> Coord2 -> Int
-solveA s pos = (`div` 2) $ length $ getLoop s [pos]
+solveA s pos = (`div` 2) $ length loop
+  where loop = getLoop s [pos]
 
 solveB :: Sketch -> Coord2 -> Int
 solveB s pos = length $ getInside s loop
@@ -53,21 +65,21 @@ solveB s pos = length $ getInside s loop
 
 getLoop :: Sketch -> [Coord2] -> [Coord2]
 getLoop s seen
-  | goesN here && stepN pos `notElem` seen = getLoop s (stepN pos:seen)
-  | goesS here && stepS pos `notElem` seen = getLoop s (stepS pos:seen)
-  | goesW here && stepW pos `notElem` seen = getLoop s (stepW pos:seen)
-  | goesE here && stepE pos `notElem` seen = getLoop s (stepE pos:seen)
+  | goesN (s!pos) && stepN pos `notElem` seen = getLoop s (stepN pos:seen)
+  | goesS (s!pos) && stepS pos `notElem` seen = getLoop s (stepS pos:seen)
+  | goesW (s!pos) && stepW pos `notElem` seen = getLoop s (stepW pos:seen)
+  | goesE (s!pos) && stepE pos `notElem` seen = getLoop s (stepE pos:seen)
   | otherwise = seen
-  where here = fromJust $ safeIndex s pos
-        pos = head seen
+  where pos = head seen
 
 getInside :: Sketch -> [Coord2] -> [Coord2]
 getInside s loop = go (0,0) False
-  where (maxr, maxc) = bounds s
+  where (maxr, maxc) = snd $ bounds s
+        go :: Coord2 -> Bool -> [Coord2]
         go (r,c) inside
           | r>=maxr = []
           | c>=maxc = go (r+1, 0) False
-          | (r,c) `elem` loop && (fromJust $ safeIndex s (r,c)) `elem` [NS, NW, NE] = go (r,c+1) (not inside)
+          | (r,c) `elem` loop && (s!(r,c)) `elem` [NS, NW, NE] = go (r,c+1) (not inside)
           | (r,c) `elem` loop || not inside = go (r,c+1) inside
           | otherwise = (r,c) : go (r,c+1) inside
 
--- src/Day11.hs
+++ src/Day11.hs
@@ -1,8 +1,51 @@
+{-# LANGUAGE TupleSections #-}
+
 module Day11 where
 
 import Paths_aoc2023 (getDataFileName)
+import Data.List (tails, elemIndices)
+import Control.Arrow ((***))
+import Data.Ix (Ix, inRange)
+import Data.Tuple (swap)
+
+type Galaxy = (Int, Int)
 
 day11 :: IO ()
 day11 = do
   contents <- init . lines <$> (getDataFileName "day11.txt" >>= readFile)
-  putStrLn $ unlines contents
+  let gals = galaxyList contents
+  let bnds = bounds contents
+  let (ers, ecs) = empties gals bnds
+  print $ solve partA ers ecs gals
+  print $ solve partB ers ecs gals
+
+galaxyList :: [[Char]] -> [Galaxy]
+galaxyList = concat . go 0
+  where go _ [] = []
+        go n (r:rs) = map (n ,) (elemIndices '#' r) : go (n+1) rs
+
+empties :: [Galaxy] -> (Int, Int) -> ([Int], [Int])
+empties xs = isEmpty galrs *** isEmpty galcs 
+  where (galrs, galcs) = unzip xs
+        isEmpty set = filter (`notElem` set) . enumFromTo 0
+
+solve :: Int -> [Int] -> [Int] -> [Galaxy] -> Int
+solve part ers ecs gals = sum $ map (uncurry (dist part ers ecs)) $ pairs gals
+
+partA, partB :: Int
+partA = 2
+partB = 1000000
+
+dist :: Int -> [Int] -> [Int] -> Galaxy -> Galaxy -> Int
+dist scale ers ecs (ay,ax) (by,bx) = abs (ax - bx) + abs (ay - by) + extrars + extracs
+  where extracs = (*(scale-1)) . length . filter (between (ax, bx)) $ ecs
+        extrars = (*(scale-1)) . length . filter (between (ay, by)) $ ers
+
+bounds :: [[a]] -> (Int, Int)
+bounds x = (length x-1, length (head x)-1)
+
+pairs :: [a] -> [(a, a)]
+pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
+
+between :: (Ix a) => (a, a) -> a -> Bool
+between range x = inRange range x || inRange (swap range) x
