f3b303c499f4ec171adbc195449d7b3ea24b3830
Zameen Cater <zcater40@stuy.edu>
2023-12-12 04:06:49 +0000

    Improved day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,8 +1,5 @@
 module Day10 where
 
--- todo: create a new sketch highlighting only the points that are in the loop instead of a list
--- (elem is slow, ! is fast)
-
 import Paths_aoc2023 (getDataFileName)
 import Data.Maybe (fromJust)
 import Data.Array
@@ -19,7 +16,9 @@ tileList :: [Tile]
 tileList = [NS, WE, NE, NW, SW, SE, X]
 
 type Coord2 = (Int, Int)
+type Presketch = Array Coord2 (Maybe Tile)
 type Sketch = Array Coord2 Tile
+type Loop = Array Coord2 Bool
 
 day10 :: IO ()
 day10 = do
@@ -30,18 +29,22 @@ day10 = do
   print $ solveA cleaned origin
   print $ solveB cleaned origin
 
-format :: [[Char]] -> Array Coord2 (Maybe Tile)
-format xs = fmap charToTile $ array ((0,0), (maxr-1, maxc-1)) [((r,c), xs!!r!!c) | r <- [0..maxr-1], c <- [0..maxc-1]]
+format :: [[Char]] -> Presketch
+format xs = fmap charToTile $ mkArray ((0,0), (maxr-1, maxc-1)) (\(r,c) -> xs!!r!!c)
   where charToTile = flip lookup $ zip "|-LJ7F." tileList
         (maxr, maxc) = (length xs, length $ head xs)
 
-getOrigin :: Array Coord2 (Maybe Tile) -> Coord2
+mkArray :: ((Int, Int), (Int, Int)) -> ((Int, Int) -> a) -> Array (Int, Int) a
+mkArray bnds f = array bnds [((r,c), f (r,c)) | r <- [rs..re], c <- [cs..ce]]
+  where ((rs, cs), (re, ce)) = bnds
+
+getOrigin :: Presketch -> Coord2
 getOrigin xs = fromJust $ lookup Nothing $ map (\(i,e) -> (e,i)) $ assocs xs
 
-clean :: Array Coord2 (Maybe Tile) -> Coord2 -> Sketch
+clean :: Presketch -> Coord2 -> Sketch
 clean xs o = fmap fromJust $ xs // [(o, guessTile xs o)]
 
-guessTile :: Array Coord2 (Maybe Tile) -> Coord2 -> Maybe Tile
+guessTile :: Presketch -> Coord2 -> Maybe Tile
 guessTile s pos = [toN, toS, toW, toE] `lookup` mapping
   where toN = (==Just True) $ goesS <$> s ! stepN pos 
         toS = (==Just True) $ goesN <$> s ! stepS pos 
@@ -49,38 +52,31 @@ guessTile s pos = [toN, toS, toW, toE] `lookup` mapping
         toE = (==Just True) $ goesW <$> s ! stepE pos 
         mapping = zip (map (([goesN, goesS, goesW, goesE] <*>) . pure) tileList) tileList
 
-safeIndex :: Array (Int, Int) a -> Coord2 -> Maybe a
-safeIndex xs (r,c)
-  | r>=maxr || r<0 || c>=maxc || c<0 = Nothing
-  | otherwise = Just $ xs!(r,c)
-  where (maxr, maxc) = snd $ bounds xs
-
 solveA :: Sketch -> Coord2 -> Int
-solveA s pos = (`div` 2) $ length loop
-  where loop = getLoop s [pos]
+solveA s pos = (`div` 2) $ length $ filter id $ elems loop
+  where loop = getLoop s pos $ mkArray (bounds s) (const False)
 
 solveB :: Sketch -> Coord2 -> Int
 solveB s pos = length $ getInside s loop
-  where loop = getLoop s [pos]
+  where loop = getLoop s pos $ mkArray (bounds s) (const False)
 
-getLoop :: Sketch -> [Coord2] -> [Coord2]
-getLoop s seen
-  | goesN (s!pos) && stepN pos `notElem` seen = getLoop s (stepN pos:seen)
-  | goesS (s!pos) && stepS pos `notElem` seen = getLoop s (stepS pos:seen)
-  | goesW (s!pos) && stepW pos `notElem` seen = getLoop s (stepW pos:seen)
-  | goesE (s!pos) && stepE pos `notElem` seen = getLoop s (stepE pos:seen)
-  | otherwise = seen
-  where pos = head seen
+getLoop :: Sketch -> Coord2 -> Loop -> Loop
+getLoop s pos loop
+  | goesN (s!pos) && not (loop ! stepN pos) = getLoop s (stepN pos) $ loop // [(stepN pos, True)]
+  | goesS (s!pos) && not (loop ! stepS pos) = getLoop s (stepS pos) $ loop // [(stepS pos, True)]
+  | goesW (s!pos) && not (loop ! stepW pos) = getLoop s (stepW pos) $ loop // [(stepW pos, True)]
+  | goesE (s!pos) && not (loop ! stepE pos) = getLoop s (stepE pos) $ loop // [(stepE pos, True)]
+  | otherwise = loop
 
-getInside :: Sketch -> [Coord2] -> [Coord2]
+getInside :: Sketch -> Loop -> [Coord2]
 getInside s loop = go (0,0) False
   where (maxr, maxc) = snd $ bounds s
         go :: Coord2 -> Bool -> [Coord2]
         go (r,c) inside
           | r>=maxr = []
           | c>=maxc = go (r+1, 0) False
-          | (r,c) `elem` loop && (s!(r,c)) `elem` [NS, NW, NE] = go (r,c+1) (not inside)
-          | (r,c) `elem` loop || not inside = go (r,c+1) inside
+          | loop!(r,c) && (s!(r,c)) `elem` [NS, NW, NE] = go (r,c+1) (not inside)
+          | loop!(r,c) || not inside = go (r,c+1) inside
           | otherwise = (r,c) : go (r,c+1) inside
 
 stepN, stepS, stepW, stepE :: Coord2 -> Coord2
