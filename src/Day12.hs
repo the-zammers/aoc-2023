b1418af26e4a25a9b7283dc4a2696fa9c315b1a4
Zameen Cater <zcater40@stuy.edu>
2023-12-13 04:49:06 +0000

    First incomplete day: day 12

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,8 +1,12 @@
 module Day12 where
 
+-- only finished the first part, tried to solve the second part, kept having trouble,
+-- discovered that glguy's solution just doesn't work for me (not enough memory),
+-- and then mistakenly deleted the first part's work, too
+
 import Paths_aoc2023 (getDataFileName)
 
 day12 :: IO ()
 day12 = do
-  contents <- init . lines <$> (getDataFileName "day12.txt" >>= readFile)
+  contents <- init . lines <$> (getDataFileName "day12a.txt" >>= readFile)
   putStrLn $ unlines contents
