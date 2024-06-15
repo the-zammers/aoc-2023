a6ac4816fe9c6eab366e72eadb59143970a725a5
Z Cater <zcater40@stuy.edu>
2024-06-15 18:36:45 +0000

    Day 13

--- src/Day13.hs
+++ src/Day13.hs
@@ -1,8 +1,35 @@
 module Day13 where
 
 import Paths_aoc2023 (getDataFileName)
+import Data.List.Split
+import Data.List
 
 day13 :: IO ()
 day13 = do
-  contents <- init . lines <$> (getDataFileName "day13.txt" >>= readFile)
-  putStrLn $ unlines contents
+  contents <- unlines . init . lines <$> (getDataFileName "day13a.txt" >>= readFile)
+  let input = format contents
+  print $ sum (map vertLine $ format contents) + 100 * sum (map horzLine $ format contents)
+  print $ map vertLine input
+  print $ map horzLine input
+  let test = input!!0
+  let test2 = reverse $ input!!1
+  print $ test
+  print $ maximum (overlap test2 (reverse test2)) `div` 2
+  print $ negate (maximum $ overlap (reverse test2) test2) `div` 2
+
+format :: String -> [ [[Char]] ]
+format xs = splitOn [""] $ lines xs
+
+vertLine = horzLine . transpose
+
+horzLine x
+  | uppish > 0 && downish < 0 = (`div` 2) $ if abs (uppish - midpoint) < abs ((length x + downish) - (length x + 1) `div` 2) then uppish else (length x + downish)
+  | uppish > 0 = (`div` 2) $ uppish
+  | downish < 0 = (`div` 2) $ length x + downish
+  | otherwise = 0
+  where uppish  = (maximum $ overlap x (reverse x))
+        downish = negate (maximum $ overlap (reverse x) x)
+        midpoint = length x `div` 2
+
+overlap :: (Eq a) => [a] -> [a] -> [Int]
+overlap a b = map length $ intersect (inits a) (tails b)
